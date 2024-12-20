ac3079b4284e00aaf2ebd304e5873b6da473b719
wuhongru <1425636954@qq.com>
2024-12-21 04:30:26 +0800

    change:targetSdk = 34

--- build-logic/src/main/kotlin/config/ConfigurationKeys.kt
+++ build-logic/src/main/kotlin/config/ConfigurationKeys.kt
@@ -18,8 +18,8 @@ internal object ConfigurationKeys {
 
     val sdkConfiguration = SdkConfiguration(
         minSdk = 27,
-        targetSdk = 35,
-        compileSdk = 35
+        targetSdk = 34,
+        compileSdk = 34
     )
 
     val ELIGIBLE_MODULES_FOR_COVERAGE = listOf(
