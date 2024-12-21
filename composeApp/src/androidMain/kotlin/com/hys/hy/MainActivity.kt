ee05af289f79f87e543d9161a476abc9a061f0df
wuhongru <1425636954@qq.com>
2024-12-21 15:00:04 +0800

    bugfix:适配状态栏字体颜色

--- composeApp/src/androidMain/kotlin/com/hys/hy/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/hys/hy/MainActivity.kt
@@ -6,13 +6,16 @@ import android.view.ViewTreeObserver
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowInsetsControllerCompat
 import com.hys.hy.navigation.App
 import io.github.vinceglb.filekit.core.FileKit
 
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,11 +23,11 @@ class MainActivity : ComponentActivity() {
         FileKit.init(this)
 
         installSplashScreen()
-
         // 使得绘制区域延伸到屏幕边缘 包括 statusBar 和 navigationBar
         enableEdgeToEdge()
         // 设置内容
         setContent {
+            ApplySystemBars()
             App()
         }
 
@@ -40,5 +43,21 @@ class MainActivity : ComponentActivity() {
                 }
             }
         )
+    }
+
+    // 根据当前模式设置状态栏字体颜色
+    @Composable
+    fun ApplySystemBars(isDarkMode: Boolean = isSystemInDarkTheme()) {
+        val windowInsetsController = WindowInsetsControllerCompat(window, window.decorView)
+
+        LaunchedEffect(isDarkMode) {
+            if (isDarkMode) {
+                // 如果是深色模式，设置状态栏字体为浅色
+                windowInsetsController.isAppearanceLightStatusBars = false
+            } else {
+                // 如果是浅色模式，设置状态栏字体为深色
+                windowInsetsController.isAppearanceLightStatusBars = true
+            }
+        }
     }
 }
