83f6c560663c239f7a4bc4ca72e4cb06b4f69564
wuhongru <1425636954@qq.com>
2025-07-27 15:50:47 +0800

    fix: remove unnecessary alias and clean up dependencies in build.gradle.kts

--- features/login/build.gradle.kts
+++ features/login/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias( libs.plugins.kmp.library.serialization.feature.plugin)
+    alias(libs.plugins.kmp.library.serialization.feature.plugin)
     alias(libs.plugins.kmp.library.koin.feature.plugin)
     alias(libs.plugins.kmp.library.compose.feature.plugin)
 }
@@ -8,12 +8,6 @@ plugins {
 
 kotlin {
     sourceSets {
-        val desktopMain by getting
-
-        androidMain.dependencies {
-            implementation(libs.androidx.activity.compose)
-        }
-
         commonMain.dependencies {
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
--- features/pomodoro/build.gradle.kts
+++ features/pomodoro/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias( libs.plugins.kmp.library.serialization.feature.plugin)
+    alias(libs.plugins.kmp.library.serialization.feature.plugin)
     alias(libs.plugins.kmp.library.koin.feature.plugin)
     alias(libs.plugins.kmp.library.compose.feature.plugin)
 }
@@ -8,17 +8,11 @@ plugins {
 
 kotlin {
     sourceSets {
-        val desktopMain by getting
-
-        androidMain.dependencies {
-            implementation(libs.androidx.activity.compose)
-        }
 
         commonMain.dependencies {
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
             implementation(libs.navigation.compose)
-
 
 
             implementation(libs.ktor.client.core)
